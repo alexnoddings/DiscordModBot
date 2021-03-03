da3c1588ced7ae3cccd9c3d54cbd1d17b025b94c
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-03 16:50:38 +0000

    Add IPluginConfig with control for whether plugins are enabled.

--- Modules.RoleBack/RoleBackConfig.cs
+++ Modules.RoleBack/RoleBackConfig.cs
@@ -1,11 +1,15 @@
 using System;
 using Elvet.Core;
+using Elvet.Core.Config;
 using Elvet.Core.Config.Exceptions;
 
 namespace Elvet.RoleBack
 {
-    internal class RoleBackConfig : IValidateAble<RoleBackConfig>
+    internal class RoleBackConfig : IPluginConfig, IValidateAble<RoleBackConfig>
     {
+        // Enabled by default
+        public bool Enabled { get; set; } = true;
+
         public string ConnectionString { get; set; } = string.Empty;
 
         public TimeSpan RolesValidFor { get; set; } = TimeSpan.FromSeconds(-1);
