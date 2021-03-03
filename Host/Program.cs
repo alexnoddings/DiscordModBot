cc7be283714e5afdc3a7eb5c06b608e546d89f81
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-03 11:06:18 +0000

    Add bot host.

---
+++ Host/Program.cs
@@ -0,0 +1,68 @@
+using Discord.Commands;
+using Discord.WebSocket;
+using Elvet.Core.Commands;
+using Elvet.Core.Config.Extensions;
+using Elvet.Core.Plugins.Registration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Hosting = Microsoft.Extensions.Hosting.Host;
+
+namespace Elvet.Host
+{
+    /// <summary>
+    /// Entry point for the Host.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Entry point for the program.
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        /// <summary>
+        /// Creates the <see cref="IHostBuilder" />.
+        /// </summary>
+        /// <param name="args">Command-line arguments.</param>
+        /// <returns>The <see cref="IHostBuilder" /> ready to build.</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Hosting.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(ConfigureHost)
+                .ConfigureServices(ConfigureServices);
+
+        /// <summary>
+        /// Configures the <paramref name="configuration" />.
+        /// </summary>
+        /// <param name="configuration">The <see cref="IConfigurationBuilder" /> to configure.</param>
+        private static void ConfigureHost(IConfigurationBuilder configuration)
+        {
+            configuration.AddEnvironmentVariables("ELVET_");
+        }
+
+        /// <summary>
+        /// Configures services for the <see cref="IHost" />.
+        /// </summary>
+        /// <param name="host">The <see cref="HostBuilderContext" /> to use when configuring services.</param>
+        /// <param name="services">The <see cref="IServiceCollection" /> to add to.</param>
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddBotConfig();
+
+            services.AddSingleton(host.HostingEnvironment);
+
+            services.AddSingleton<DiscordSocketClient>();
+            services.AddSingleton<CommandService>();
+            services.AddCommandHandler();
+
+            services.AddHostedService<BotService>();
+
+            // Modules
+            services
+                .AddPluginRegister();
+        }
+    }
+}
