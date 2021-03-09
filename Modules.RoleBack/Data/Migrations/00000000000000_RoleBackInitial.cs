157365f57a1f0a397651d404352e3e9ac20bd153
Alex Noddings <alexnoddings@users.noreply.github.com>
2021-03-09 12:13:25 +0000

    Rename migration, add SQL file.

--- Modules.RoleBack/Data/Migrations/00000000000000_RoleBackInitial.cs
+++ Modules.RoleBack/Data/Migrations/00000000000000_RoleBackInitial.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Elvet.RoleBack.Data.Migrations
 {
-    public partial class Initial : Migration
+    public partial class RoleBackInitial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
